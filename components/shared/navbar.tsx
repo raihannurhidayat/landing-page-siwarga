33ae30a2905753bb4cd2aa98053819c1f5814129
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2026-06-07 20:10:13 +0700

    feat(navbar): enhance navbar with responsive links and improved layout feat(fonts): add Fraunces font

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,35 +1,51 @@
 import { HouseIcon } from "lucide-react";
-import { Button } from "../ui/button";
+
+import { cn } from "@/lib/utils";
+import { fraunces } from "@/lib/fonts";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <header className="flex justify-between items-center border border-b-border w-full px-36 py-4">
-      <div className="flex items-center gap-2">
+      <Link href="/" className="flex items-center gap-2 pl-2 pr-4">
         <div className="p-2 bg-primary text-primary-foreground rounded-md">
-          <HouseIcon />
+          <HouseIcon size={24} />
         </div>
-        <div className="flex flex-col items-center gap-1">
-          <h1>Logo</h1>
-          <span>name</span>
+        <div className="flex flex-col items-start gap-1">
+          <h1 className={cn(fraunces.className, "text-xl font-bold")}>Logo</h1>
+          <span className="text-xs text-muted-foreground font-semibold">
+            RT 04 RW 02
+          </span>
         </div>
-      </div>
+      </Link>
 
       <nav>
         <ul className="flex gap-4 items-center">
           <li>
-            <a href="#">Beranda</a>
+            <Button variant="ghost">
+              <Link href="#">Beranda</Link>
+            </Button>
           </li>
           <li>
-            <a href="#">Fitur</a>
+            <Button variant="ghost">
+              <Link href="#">Fitur</Link>
+            </Button>
           </li>
           <li>
-            <a href="#">Modul</a>
+            <Button variant="ghost">
+              <Link href="#">Modul</Link>
+            </Button>
           </li>
           <li>
-            <a href="#">FAQ</a>
+            <Button variant="ghost">
+              <Link href="#">FAQ</Link>
+            </Button>
           </li>
           <li>
-            <a href="#">Kontak</a>
+            <Button variant="ghost">
+              <Link href="#">Kontak</Link>
+            </Button>
           </li>
         </ul>
       </nav>
