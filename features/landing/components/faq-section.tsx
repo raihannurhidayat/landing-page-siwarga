0a93813d1b3679d14dab5d4380dae82e162fb248
raihannurhidayat <grandrx041@gmail.com>
2026-06-07 20:19:37 +0700

    feature(section): Completed FAQ Section

--- features/landing/components/faq-section.tsx
+++ features/landing/components/faq-section.tsx
@@ -1,6 +1,13 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Accordion as AccordionPrimitive } from "radix-ui";
 import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
-import { CircleQuestionMark } from "lucide-react";
+import { CircleQuestionMark, MinusIcon, PlusIcon } from "lucide-react";
 import React from "react";
 
 const FAQItems = [
@@ -31,15 +38,41 @@ const FAQItems = [
   },
 ];
 
+const AccordionPlusMinusIconDemo = () => {
+  return (
+    <Accordion
+      type="single"
+      collapsible
+      className='w-full border-0 [&>*>[data-slot="accordion-content"]]:px-0'
+      defaultValue="item-1"
+    >
+      {FAQItems.map((item, index) => (
+        <AccordionItem key={index} value={`item-${index + 1}`}>
+          <AccordionPrimitive.Header className="flex">
+            <AccordionPrimitive.Trigger
+              data-slot="accordion-trigger"
+              className="focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-center justify-between gap-4 border border-transparent px-2 py-2.5 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-3 disabled:pointer-events-none disabled:opacity-50"
+            >
+              {item.question}
+              <PlusIcon className="text-muted-foreground pointer-events-none block size-4 shrink-0 transition-transform duration-500 in-data-open:hidden" />
+              <MinusIcon className="text-muted-foreground pointer-events-none hidden size-4 shrink-0 transition-transform duration-500 in-data-open:block" />
+            </AccordionPrimitive.Trigger>
+          </AccordionPrimitive.Header>
+          <AccordionContent className="text-muted-foreground px-2">
+            {item.answer}
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+};
+
 const FAQSection = () => {
   return (
-    <section>
-      <div>
+    <section className="py-24">
+      <div className="mx-auto max-w-275 px-6">
         <div className="text-center">
-          {/* <SectionLabel className="mb-[14px]" icon={CircleQuestionMark}>
-            FAQ
-          </SectionLabel> */}
-          <Badge className="bg-primary/60">
+          <Badge className="inline-flex items-center gap-1.5 rounded-[20px] border px-3.5 py-1.25 text-[12px] font-semibold uppercase tracking-[0.4px] border-primary/20 bg-secondary text-primary">
             <CircleQuestionMark /> <span className="text-primary">FAQ</span>
           </Badge>
           <h2
@@ -51,6 +84,21 @@ const FAQSection = () => {
             <br />
             <em className="italic text-primary">sering ditanyakan</em>
           </h2>
+        </div>
+
+        <div className="mx-auto mt-12 max-w-175">
+          <Accordion type="single" collapsible>
+            {FAQItems.map((item, index) => (
+              <AccordionItem key={index} value={`item-${index + 1}`}>
+                <AccordionTrigger className="text-[16px]">
+                  {item.question}
+                </AccordionTrigger>
+                <AccordionContent className="text-[14px]">
+                  {item.answer}
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
         </div>
       </div>
     </section>
